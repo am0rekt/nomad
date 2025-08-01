4aff15ceb415f8f46a1def62a41a167466a24066
arjun <arjun@LAPTOP-BKPVDH7C>
2025-08-01 22:40:10 +0530

    git push origin master

--- src/main/java/com/nomadnetwork/controller/UserViewController.java
+++ src/main/java/com/nomadnetwork/controller/UserViewController.java
@@ -1,10 +1,14 @@
 package com.nomadnetwork.controller;
 
+import com.nomadnetwork.entity.User;
+import com.nomadnetwork.enums.Role;
 import com.nomadnetwork.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserViewController {
@@ -17,4 +21,21 @@ public class UserViewController {
         model.addAttribute("users", userService.getAllUsers());
         return "users"; // Refers to users.html
     }
+    
+
+    @GetMapping("/create-user")
+    public String showCreateUserForm(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("roles", Role.values()); // use enum values
+        return "create_user";
+    }
+
+    
+    @PostMapping("/create-user")
+    public String handleCreateUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/user-page"; // After form submit, go to list
+    }
+
+
 }
